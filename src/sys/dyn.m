74a1dced9afa0ffb6b020069b08ef82875155d4a
Antonio Terpin <antonio.terpin@gmail.com>
2021-06-08 15:43:11 +0200

    fixed dyn

--- src/sys/dyn.m
+++ src/sys/dyn.m
@@ -5,21 +5,19 @@ function dxdt = dyn(x, u)
 %       u -- 2Nx1 agents input
 
 N = numel(x) / 3;
-k = [.1, -.1];
+k = [.1, .1];
 velSat = [-.1, .1];
 
 % llc
 ubar = zeros(2,N);
 states = reshape(x,3,N);
 delta = reshape(u,2,N) - states(1:2,:);
-if any(delta ~= 0)
-    theta = states(3,:) - atan2(delta(2,:), delta(1,:));
-    ubar(1,:) = k(1) * vecnorm(delta,2,1) .* cos(theta);
-    ubar(2,:) = -k(2) * theta;
-end
+theta = states(3,:) - atan2(delta(2,:), delta(1,:));
+ubar(1,:) = k(1) * vecnorm(delta,2,1) .* cos(theta);
+ubar(2,:) = -k(2) * theta;
 
 % input saturation
-vel = max(velSat(1), min(velSat(2), ubar(1,:)));
+vel = ubar(1,:);%max(velSat(1), min(velSat(2), ubar(1,:)));
 angVel = ubar(2,:);
 
 % dxdt
